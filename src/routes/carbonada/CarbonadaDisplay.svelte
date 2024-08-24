1180d76388ab524cd24fdfa1d4e376ec7a495800
Vokturz <victornavarrokp47@gmail.com>
2024-08-24 11:58:17 -0400

    Move card list to separate component for better modularity

--- src/routes/carbonada/CarbonadaDisplay.svelte
+++ src/routes/carbonada/CarbonadaDisplay.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { createEventDispatcher } from 'svelte';
-    import { carbonada, draggingCard, progress, gameState } from './stores';
+    import { draggingCard } from './stores';
 	import { placeCard } from './carbonadaLogic';
 	import ProgressBar from './ProgressBar.svelte';
+	import CardList from './CardList.svelte';
   
     let hoveringIndex: number | null = null;
   
@@ -28,16 +29,7 @@
 </script>
 
 
-  	<div class="cards-dropped-list">
-		<h4>Carbon footprint of dropped Items</h4>
-        <ul>
-            {#each $carbonada as card (card.name)}
-                <li>
-                    <b>{card.name}</b>: {card.carbonFootprint.toFixed(2)} CO2e
-                </li>
-            {/each}
-        </ul>
-	</div>
+  <CardList />
   <div class="carbonada-container">
     <div class="carbonada">
       <div class="drop-zone"
@@ -78,37 +70,6 @@
 	  min-height: 200px;
 	  justify-content: center;
 	}
-  
-    .cards-dropped-list {
-		position: absolute;
-		left: 10px;
-		top: 10px;
-		bottom: 0;
-		width: 20%;
-		display: flex;
-		flex-direction: column;
-		padding: 50px;
-		/* background-color: #f9f9f9; */
-		border-right: 1px solid #ddd;
-		overflow-y: auto;
-		z-index: 1;
-    }
-
-    .cards-dropped-list h4 {
-        margin-top: 0;
-        margin-bottom: 10px;
-    }
-
-    .cards-dropped-list ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-    }
-
-    .cards-dropped-list li {
-        margin-bottom: 5px;
-        font-size: 14px;
-    }
   
 	.drop-zone {
 	  /* width: 20px; */
---
+++ src/routes/carbonada/CardList.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { carbonada } from './stores';
+</script>
+
+<div class="cards-dropped-list">
+    <h4>Carbon footprint of dropped Items</h4>
+    <ul>
+        {#each $carbonada as card (card.name)}
+            <li>
+                <b>{card.name}</b>: {card.carbonFootprint.toFixed(2)} CO2e
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+
+    .cards-dropped-list {
+            position: absolute;
+            left: 10px;
+            top: 10px;
+            bottom: 0;
+            width: 20%;
+            display: flex;
+            flex-direction: column;
+            padding: 50px;
+            /* background-color: #f9f9f9; */
+            border-right: 1px solid #ddd;
+            overflow-y: auto;
+            z-index: 1;
+        }
+
+    .cards-dropped-list h4 {
+        margin-top: 0;
+        margin-bottom: 10px;
+    }
+
+    .cards-dropped-list ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .cards-dropped-list li {
+        margin-bottom: 5px;
+        font-size: 14px;
+    }
+
+</style>
+
